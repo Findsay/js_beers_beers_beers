1553a46e43359bf5d8516045d7155a3095d3f2c6
Findsay <fiona.e.lindsay@gmail.com>
2017-12-06 15:28:02 +0000

    make Request function added

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
-var app = function(){
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+}
 
+
+
+
+var app = function(){
+  loadBeers();
 }
 
 window.addEventListener('load', app);
